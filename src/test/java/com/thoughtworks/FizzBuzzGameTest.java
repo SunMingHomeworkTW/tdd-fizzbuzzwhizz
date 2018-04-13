e9d0ff03d7b8b05201dfab1bd6154e4d8ad3f44e
SunMing <1229536801@qq.com>
2018-04-13 21:03:28 +0800

    根据题目要求完成测试用例

--- src/test/java/com/thoughtworks/FizzBuzzGameTest.java
+++ src/test/java/com/thoughtworks/FizzBuzzGameTest.java
@@ -1,10 +1,71 @@
 package com.thoughtworks;
 
+import static org.junit.Assert.*;
+
+import org.junit.Before;
 import org.junit.Test;
 
 public class FizzBuzzGameTest {
+    private int[] specialNums;
+    private FizzBuzzGame fizzBuzzGame;
+
+    @Before
+    public void setUp(){
+        specialNums=new int[]{3,5,7};
+        fizzBuzzGame=new FizzBuzzGame();
+    }
+
+    @Test
+    public void should_return_Fizz_when_is_multiple_of_3() {
+        assertEquals(fizzBuzzGame.getResult(6),"Fizz");
+        assertEquals(fizzBuzzGame.getResult(9),"Fizz");
+        assertEquals(fizzBuzzGame.getResult(99),"Fizz");
+    }
+
+    @Test
+    public void should_return_Buzz_when_is_multiple_of_5() {
+        assertEquals(fizzBuzzGame.getResult(10),"Buzz");
+        assertEquals(fizzBuzzGame.getResult(25),"Buzz");
+        assertEquals(fizzBuzzGame.getResult(55),"Buzz");
+    }
+
+    @Test
+    public void should_return_Whizz_when_multiple_of_7() {
+        assertEquals(fizzBuzzGame.getResult(14),"Whizz");
+        assertEquals(fizzBuzzGame.getResult(49),"Whizz");
+        assertEquals(fizzBuzzGame.getResult(77),"Whizz");
+    }
+
+    @Test
+    public void should_return_Fizz_when_is_multiple_of_3_and_5() {
+        assertEquals(fizzBuzzGame.getResult(15),"FizzBuzz");
+        assertEquals(fizzBuzzGame.getResult(45),"FizzBuzz");
+    }
+
+    @Test
+    public void should_return_Fizz_when_is_multiple_of_3_and_7() {
+        assertEquals(fizzBuzzGame.getResult(21),"FizzWhizz");
+        assertEquals(fizzBuzzGame.getResult(63),"FizzWhizz");
+    }
+
+    @Test
+    public void should_return_Fizz_when_is_multiple_of_5_and_7() {
+        assertEquals(fizzBuzzGame.getResult(35),"BuzzWhizz");
+        assertEquals(fizzBuzzGame.getResult(175),"BuzzWhizz");
+    }
+
+    @Test
+    public void should_return_Fizz_when_is_multiple_of_3_5_and_7() {
+        assertEquals(fizzBuzzGame.getResult(105),"FizzBuzzWhizz");
+    }
+
     @Test
     public void should_return_Fizz_when_have_3() {
-
+        assertEquals(fizzBuzzGame.getResult(3),"Fizz");
+        assertEquals(fizzBuzzGame.getResult(13),"Fizz");
+        assertEquals(fizzBuzzGame.getResult(35),"Fizz");
+        assertEquals(fizzBuzzGame.getResult(37),"Fizz");
+        assertEquals(fizzBuzzGame.getResult(53),"Fizz");
+        assertEquals(fizzBuzzGame.getResult(73),"Fizz");
     }
 }
